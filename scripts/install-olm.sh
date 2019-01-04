fce10306a24a2d3836099628621f0313b643b1c9
trilliams <tribecca@tribecc.us>
2019-01-04 11:49:30 -0600

    fixed typo in image puller

--- scripts/install-olm.sh
+++ scripts/install-olm.sh
@@ -4,4 +4,4 @@
 docker pull quay.io/coreos/olm:master-73bc1d54
 docker tag quay.io/coreos/olm:master-73bc1d54 quay.io/coreos/olm:latest
 docker pull quay.io/coreos/catalog:master-57dd618d
-docker tag quay.io/coreos/catalog:latest
+docker tag quay.io/coreos/catalog:master-57dd618d quay.io/coreos/catalog:latest
