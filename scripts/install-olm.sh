e220d08ad3ed6b1178bef7a098eab95dcdb4ed91
trilliams <tribecca@tribecc.us>
2019-01-07 09:24:28 -0600

    updated hosts & image puller

--- scripts/install-olm.sh
+++ scripts/install-olm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # script for prepopulating Docker images needed for OLM
-
+curl -o /etc/ansible/hosts https://raw.githubusercontent.com/linuxacademy/content-deploy-an-operator/master/etc/ansible/hosts
 docker pull quay.io/coreos/olm:master-73bc1d54
 docker tag quay.io/coreos/olm:master-73bc1d54 quay.io/coreos/olm:latest
 docker pull quay.io/coreos/catalog:master-57dd618d
--- scripts/installer2.sh
+++ scripts/installer2.sh
@@ -2,9 +2,12 @@
 # installer script for OLM on OpenShift v3.11
 echo "Please wait while installer completes. The process may take 5-10
 minutes." >> /etc/motd
+git clone https://github.com/operator-framework/operator-lifecycle-manager
+oc create -f /root/operator-lifecycle-manager/
 rm -rf /etc/ansible/hosts
 curl -o /etc/ansible/hosts https://raw.githubusercontent.com/linuxacademy/content-deploy-an-operator/master/etc/ansible/hosts
 curl -o /var/tmp/images.sh https://raw.githubusercontent.com/linuxacademy/content-deploy-an-operator/master/scripts/install-olm.sh
 /bin/bash /var/tmp/images.sh
 sleep 5
-ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/olm/config.yml && echo "Installation complete! You may now begin your lab."
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/olm/config.yml
+echo "Installation complete! You may now begin your lab."
